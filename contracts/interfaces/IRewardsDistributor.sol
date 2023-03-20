bd80ba916fb3f7b46a37d9dca03f2166576c2e0a
kostissss <kostevage@Gmail.com>
2023-03-20 21:26:44 +0200

    beehive-contracts-v1

---
+++ contracts/interfaces/IRewardsDistributor.sol
@@ -0,0 +1,9 @@
+pragma solidity 0.8.13;
+
+interface IRewardsDistributor {
+  function checkpoint_token() external;
+
+  function checkpoint_total_supply() external;
+
+  function claim(uint _tokenId) external returns (uint);
+}
